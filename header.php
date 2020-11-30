8c2d12b3d709c99e737cd9b3c77b58129a469646
CatNq <gaumeochit162@gmail.com>
2020-11-30 12:29:19 +0700

    clean up signup, login

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="contact.php">Contact</a></li>
-            <li><a href="index.php">About</a></li>
+            <li><a href="about.php">About</a></li>
         </ul>
         <div class="header-login">
 
