dc6f762bb85743234a9c3ed43046a2fe91b11f34
CatNq <gaumeochit162@gmail.com>
2020-12-04 00:07:52 +0700

    implement bootstrap4

--- header.php
+++ header.php
@@ -9,11 +9,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <header>
-    <nav class="nav-header-main">
+    <nav class="container-fluid">
         <a href="index.php" class="header-logo">
             <img src="img/logo-1.png" alt="logo">
         </a>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     include 'header.php';
 ?>
     <main>
-        <div class="wrapper-main">
+        <div class="container">
             <section class="section-default">
 
                 <?php
@@ -15,6 +15,17 @@
                         echo '<p class="login-status">You are not logged in, Please login to continue!</p>';
                     }
                 ?>
+            </section>
+            <section class="center-main">
+                    <div>
+                        <div class="card">
+                            <img src="img/logo-1.png" alt="Avatar" style="width:50%">
+                            <div class="container">
+                                <h4><b>test</b></h4>
+                                <p>Course-1</p>
+                            </div>
+                        </div>
+                    </div>
             </section>
         </div>
     </main>
