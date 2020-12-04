7ecf5dfb3a6ed5528dffa0a9b57e4cbed3f333a1
CatNq <38415617+CatNq@users.noreply.github.com>
2020-12-04 15:00:16 +0700

    add getcourse

--- page.php
+++ page.php
@@ -10,22 +10,37 @@
 
 <main>
     <section class="container row bg-light">
-        <div>
             <div class="card col">
-                <img src="img/logo-1.png" alt="Avatar" style="width:100%">
+                <img src="img/logo-1.png" alt="Avatar" style="width:50%">
+                <div>
+                    <h4><b>test</b></h4>
+                    <p>Course-1</p>
+                    <form action="includes/getcourse.inc.php">
+                        <button type="submit-takecourse" name="take-course">Take this course</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card col">
+                <img src="img/logo-1.png" alt="Avatar" style="width:50%">
                 <div>
                     <h4><b>test</b></h4>
                     <p>Course-1</p>
                 </div>
             </div>
             <div class="card col">
-                <img src="img/logo-1.png" alt="Avatar" style="width:100%">
+                <img src="img/logo-1.png" alt="Avatar" style="width:50%">
                 <div>
                     <h4><b>test</b></h4>
                     <p>Course-1</p>
                 </div>
             </div>
-        </div>
+            <div class="card col">
+                <img src="img/logo-1.png" alt="Avatar" style="width:50%">
+                <div>
+                    <h4><b>test</b></h4>
+                    <p>Course-1</p>
+                </div>
+            </div>
    </section>
 
 </main>
--- tmp.php
+++ tmp.php
@@ -7,6 +7,13 @@ CREATE TABLE users (
     user_password varchar(256) not null
 );
 
+CREATE TABLE courses (
+	idCourses int(11) not null AUTO_INCREMENT PRIMARY KEY,
+    titleCourses varchar(256) not null,
+    contentCourses varchar(256) not null,
+    linkCourses varchar(256) not null,
+);
+
 INSERT INTO users (user_first, user_last, user_email, user_uid, user_password)
     VALUES ('first', 'last', 'a@gmail.com', 'cat', 'cat');
 
